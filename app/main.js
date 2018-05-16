b7ee8acdc7c8ae4d07043c81d328015f4a58786a
JakeBrose <mordecaistudios@gmail.com>
2018-05-15 20:32:15 -0500

    updated routes and nav

--- app/main.js
+++ app/main.js
@@ -1,9 +1,21 @@
 'use strict';
 
 angular.module("portfolio",["ngRoute"])
-    .config($routeProvider => {
-        $routeProvider
-            .when("/", {
-                templateUrl: "partials/home.html",
-            })
+  .config($routeProvider => {
+    $routeProvider
+      .when("/", {
+        templateUrl: "partials/home.html",
+      })
+      .when("/resume", {
+        templateUrl: "partials/resume.html",
+      })
+      .when("/portfolio", {
+        templateUrl: "partials/portfolio.html",
+      })
+      .when("/about", {
+        templateUrl: "partials/about.html",
+      })
+      .when("/contact", {
+        templateUrl: "partials/contact.html",
+      })
     });
